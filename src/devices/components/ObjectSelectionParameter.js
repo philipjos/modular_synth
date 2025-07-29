90b489c5af765367cb8297771b9545f991d7872a
Philip Andersen <philipandersen@Philips-MacBook-Air.local>
2025-07-29 12:22:43 -0500

    Tie an id to objects in selectors to persist selection

--- src/devices/components/ObjectSelectionParameter.js
+++ src/devices/components/ObjectSelectionParameter.js
@@ -3,30 +3,63 @@ class ObjectSelectionParameter extends SelectionParameter {
         super(typeId, typeDisplayName, undefined, modulatable)
         
         this.objectsForOptions = []
+        this.idObjectMap = {}
+        this.lastId = undefined
     }
     addOptionFromObject(object) {
-        const option = this.addObjectAndGenerateOption(object, newIndex)
+        const option = this.addObjectAndGenerateOption(object)
         this.addOptionAndUpdateDropdown(option)
     }
 
     setOptionsFromObjectsAndUpdateDropdown(objects) {
         this.objectsForOptions = []
-        const options = objects.map((e) => this.addObjectAndGenerateOption(e))
+        const options = objects.map((e) => {
+            return this.addObjectAndGenerateOption(e)
+        })
         this.setOptionsAndUpdate(options)
     }
 
     addObjectAndGenerateOption(object) {
-        const newIndex = this.objectsForOptions.length
+        let id = undefined
+        let idFound = false
+        let i = 0
+
+        let ids = Object.keys(this.idObjectMap)
+        while (!idFound && i < ids.length) {
+            let idToCompare = ids[i]
+            if (this.idObjectMap[idToCompare] === object) {
+                id = i
+                idFound = true
+            }
+
+            i += 1
+        }
+
+        if (id === undefined) {
+            if (this.lastId === undefined) {
+                id = 0
+            } else {
+                id = this.lastId + 1
+            }
+
+            this.idObjectMap[id] = object
+
+            this.lastId = id
+        }
+
         this.objectsForOptions.push(object)
-        const option = this.getOptionFromObject(object, newIndex)
+        const option = this.getOptionFromObject(
+            object, 
+            id
+        )
 
         return option
     }
 
-    getOptionFromObject(object, index) {
+    getOptionFromObject(object, id) {
         return {
             label: object.displayName,
-            value: index
+            value: id
         }
     }
 
